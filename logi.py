4fa40de739886050961f6736245cd60aae113bda
Yann Staggl <76598692+ystaggl@users.noreply.github.com>
2022-04-04 08:22:24 +0200

    Update logi.py

--- logi.py
+++ logi.py
@@ -4,14 +4,17 @@ Logipy: An up-to-date Logitech Illumination SDK Wrapper for python!
 
 import os
 import ctypes
-import LogiLib as Lib
+import logilib as lib
 import atexit
 import struct
 import time
-DLLPath = os.getcwd() + f"\\LogitechLedEnginesWrapper{(8 * struct.calcsize('P'))}.dll" #Decides which DLL to use
-Led = ctypes.WinDLL(DLLPath)
-Led.LogiLedInit()
-Led.LogiLedSetTargetDevice(ctypes.c_int(4)) #Sets device to keyboard
+if 8 * struct.calcsize('P') == 64:
+    dll_path = os.getcwd() + "\\Led\\Lib\\LogitechLedEnginesWrapper\\x64\\LogitechLedEnginesWrapper.dll"
+else:
+    dll_path = os.getcwd() + "\\Led\\Lib\\LogitechLedEnginesWrapper\\x86\\LogitechLedEnginesWrapper.dll"
+led = ctypes.WinDLL(dll_path)
+led.LogiLedInit()
+led.LogiLedSetTargetDevice(ctypes.c_int(4)) #Sets device to keyboard
 
 def LightSingleKey(*args):
     """
@@ -21,8 +24,8 @@ def LightSingleKey(*args):
         where keyName is the name of the Key, and red, green, and blue are RGB values out of 100
     """
 
-    runcommand = "Led.LogiLedSetLightingForKeyWithKeyName("
-    parse_and_execute(runcommand, *args)
+    run_command = "Led.LogiLedSetLightingForKeyWithKeyName("
+    parse_and_execute(run_command, *args)
     return
 
 #Add New Aliases Here
@@ -42,11 +45,11 @@ def __getattr__(command):
                 command += "()"
             exec("Led.LogiLed" + command)
             return "Ran Command with no args"
-        runcommand = "Led.LogiLed" + command + "("
-        parse_and_execute(runcommand, *args)
+        run_command = "Led.LogiLed" + command + "("
+        parse_and_execute(run_command, *args)
     return method
 
-def parse_and_execute(runcommand, *args):
+def parse_and_execute(run_command, *args):
     """
     This function parses and executes the command. Not intended to be called directly, only called by other functions.
     """
@@ -57,21 +60,21 @@ def parse_and_execute(runcommand, *args):
                 arg = a.split(" ")
                 if hasattr(Lib, arg[0]):
                     arg[1] = "[\'" + arg[1] + "\']"
-                    runcommand += "ctypes.c_int(" + f"Lib.{arg[0]}{arg[1]}" + "),"
+                    run_command += "ctypes.c_int(" + f"Lib.{arg[0]}{arg[1]}" + "),"
                 else:
                     print(f"Encountered string {arg[0]}, which isn't an attribute of LogiLib.py. This is probably a bug.")
                     return
             else:
                 a = "[\'" + a + "\']"
-                runcommand += "ctypes.c_int(" + f"Lib.KeyName{a}" + "),"
+                run_command += "ctypes.c_int(" + f"Lib.KeyName{a}" + "),"
                 print("String with no dictionary detected. Assuming dictionary KeyName.")
         if type(a) == type(1):
-            runcommand += str(f"ctypes.c_int({a})") + ","
-    runcommand = runcommand[:-1] + ")"
+            run_command += str(f"ctypes.c_int({a})") + ","
+    run_command = run_command[:-1] + ")"
     try:
-        exec(runcommand)
+        exec(run_command)
     except Exception:
-        print(f"An Error has occured. The script attempted to run the command {runcommand}. Origin: parse_and_execute")
+        print(f"An Error has occured. The script attempted to run the command {run_command}. Origin: parse_and_execute")
     return
 
 def exit_handler():
